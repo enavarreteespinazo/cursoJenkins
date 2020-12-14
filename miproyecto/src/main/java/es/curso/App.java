1f50f30236d9b0dbca4d61242500e02daab182d1
Ubuntu <ubuntu@ip-172-31-1-87.eu-west-1.compute.internal>
2020-12-14 18:52:16 +0000

    pruebas

--- miproyecto/src/main/java/es/curso/App.java
+++ miproyecto/src/main/java/es/curso/App.java
@@ -13,15 +13,17 @@ public class App
     
     public static int doblar( int numero )
     {
-        return  numero*2;
+        return numero*2;
     }
-    
     public static int mayor( int numero1, int numero2 )
     {
-       if (numero1 > numero2){
-           return numer1;
-       }
-       return numero2;
+        /*if (numero1 > numero2) {
+            return numero1
+        }
+        return numero2;
+        */
+        return numero1 > numero2 ? numero1 : numero2 ;
     }
+
     
 }
